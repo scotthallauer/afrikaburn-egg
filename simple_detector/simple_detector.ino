98d54ed2b3c49b8b8be0c7aed134bce051730575
Scott Hallauer <scohals@amazon.com>
2026-04-19 12:29:38 +0200

    Move scripts in separate folders

---
+++ simple_detector/simple_detector.ino
@@ -0,0 +1,23 @@
+const int sensorPin = A0;   // The LD2410C OUT pin
+const int ledPin = 9;       // Your LED connected to PWM pin D9
+
+int ledBrightness = 0;
+
+void setup() {
+  Serial.begin(9600);      
+  pinMode(sensorPin, INPUT); 
+  pinMode(ledPin, OUTPUT); 
+  Serial.println("System Online");
+}
+
+void loop() {
+  int sensorState = digitalRead(sensorPin); 
+
+  if (sensorState == HIGH && ledBrightness < 255) {
+    analogWrite(ledPin, ledBrightness++);
+  } else if (sensorState == LOW && ledBrightness > 0) {
+    analogWrite(ledPin, ledBrightness--);
+  }
+
+  delay(10); 
+}
